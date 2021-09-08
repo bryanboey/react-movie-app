a4fa77d10317a40d6388b8f04028ad1cecf01e1e
bryanboey <bryanboey@gmail.com>
2021-09-08 23:27:58 +0800

    button tooltip

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "../context/GlobalState";
+import ReactTooltip from "react-tooltip";
 
 export default function Button({ movie }) {
 	const {
@@ -20,13 +21,29 @@ export default function Button({ movie }) {
 		<>
 			{watchListMovie ? (
 				<>
+					<ReactTooltip
+						id="eye-slash"
+						type="dark"
+						effect="solid"
+						delayShow={300}
+					/>
 					<button
+						data-tip="remove from watchlist"
+						data-for="eye-slash"
 						className="btn btn-sm eye-slash-btn overlay-btn btn-dark"
 						onClick={() => removeFromWatchList(movie.id)}
 					>
 						<i className="fas fa-eye-slash"></i>
 					</button>
+					<ReactTooltip
+						id="check-circle"
+						type="dark"
+						effect="solid"
+						delayShow={300}
+					/>
 					<button
+						data-tip="move to completed"
+						data-for="check-circle"
 						className="btn btn-sm check-circle-btn overlay-btn btn-dark"
 						onClick={() => moveToCompletedList(movie)}
 					>
@@ -35,7 +52,15 @@ export default function Button({ movie }) {
 				</>
 			) : completedListMovie ? (
 				<>
+					<ReactTooltip
+						id="minus-circle"
+						type="dark"
+						effect="solid"
+						delayShow={300}
+					/>
 					<button
+						data-tip="remove from completed"
+						data-for="minus-circle"
 						className="btn btn-sm minus-circle-btn overlay-btn btn-dark"
 						onClick={() => removeFromCompletedList(movie.id)}
 					>
@@ -44,13 +69,29 @@ export default function Button({ movie }) {
 				</>
 			) : (
 				<>
-                    <button 
-                        className="btn btn-sm eye-btn overlay-btn btn-dark" 
-                        onClick={() => addToWatchList(movie)}
-                    >
-                        <i className="fas fa-eye"></i>
-                    </button>
+					<ReactTooltip
+						id="eye"
+						type="dark"
+						effect="solid"
+						delayShow={300}
+					/>
 					<button
+						data-tip="want to watch"
+						data-for="eye"
+						className="btn btn-sm eye-btn overlay-btn btn-dark"
+						onClick={() => addToWatchList(movie)}
+					>
+						<i className="fas fa-eye"></i>
+					</button>
+					<ReactTooltip
+						id="check-circle-2"
+						type="dark"
+						effect="solid"
+						delayShow={300}
+					/>
+					<button
+						data-tip="add to completed"
+						data-for="check-circle-2"
 						className="btn btn-sm check-circle-btn overlay-btn btn-dark"
 						onClick={() => addToCompletedList(movie)}
 					>
